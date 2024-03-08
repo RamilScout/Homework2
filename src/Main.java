72d6658993b53429b1645d316f8e702d5dfecb2b
RamilScout <ramilrakhmatulinscout@gmail.com>
2024-03-08 16:56:09 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        var box = 5;
+        System.out.println(box);
+        System.out.println("Hello world");
+        var capacityleft = 40;
+        System.out.println("Можно положить " + capacityleft + " кг вещей");
+        var a = 4;
+        var b = 5;
+
+        var result1 = a + b;
+        var result2 = a * b;
+        var result3 = a / b;
+        var result = a / 2 * b;
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result);
+
+        var name = "Рамиль";
+        System.out.println("Меня зовут " + name + " и мне " + 31 + " год");
+
+
+
+
+
     }
 }
